53e3583850713b0987e1a095cef5cf4a20630536
Fotoweck <90695640+Fotoweck@users.noreply.github.com>
2021-12-25 12:27:13 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,33 @@
+#number 2
+n=int(input())
+list=[None]*n
+print(list)
+
+
+#number 3
+#a=[1,2,3,4,5,6,7,8,9,10]
+#n=len(a)
+#print(a[0:n:n-1])
+
+
+
+#NUMBER 1 (EXAMPLES)
+#n=0
+#while (n<10):
+    #print('MAI')
+    #n+=1
+
+#for i in range(0,11):
+    #print('MAI')
+
+#for i in range(0,11):
+    #if i==5:
+         #break
+    #print('MAI')
+
+#for i in range(0,11):
+    #if i==5:
+         #continue
+    #print(i**2)
+
+
